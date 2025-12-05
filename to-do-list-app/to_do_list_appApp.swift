7d9343e36c9e31841f399ef5e3106e6f3e8faedb
Ru-2008 <ranarudrarajsinh20@gmail.com>
2025-12-05 14:07:18 +0530

    Initial Commit

---
+++ to-do-list-app/to_do_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_list_appApp.swift
+//  to-do-list-app
+//
+//  Created by students on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct to_do_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
